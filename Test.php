1deb67d16460d63781b412965e37ca5598d1f0ea
askahoward <askahoward@gmail.com>
2016-06-18 15:27:22 +0800

    php

---
+++ Test.php
@@ -0,0 +1,19 @@
+﻿<?php
+	date_default_timezone_set("Asia/Taipei");
+	
+	list($s1,$s2)=explode(' ',microtime());
+    $data = date("Y-m-d H:i:s");
+    $message_content_id = date('Ymd') . date('His') . substr($s1,2,6);
+	
+	echo date("Y/m/d H:i:s");
+	echo "<br>";
+	echo strtotime(date("Y/m/d H:i:s"));
+	echo "<br>";
+	echo strtotime("now") . substr($s1,2,3);
+	echo "<br>";
+	echo microtime();
+	echo "<br>";
+	echo substr($s1,2,6);
+	
+?>
+
